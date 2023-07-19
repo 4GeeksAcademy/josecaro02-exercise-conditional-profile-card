767132885c452a3e9ba70b8db00cd5f6fa409468
Jose David Caro Cantor <43614556+josecaro02@users.noreply.github.com>
2023-07-19 15:52:26 +0000

    Add solved task

--- src/js/app.js
+++ src/js/app.js
@@ -32,12 +32,24 @@ function render(variables = {}) {
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
+          <img src="${
+            variables.avatarURL == null
+              ? "https://randomuser.me/api/portraits/women/42.jpg"
+              : variables.avatarURL
+          }" class="photo" />
+          <h1>${variables.name == null ? "Jose" : variables.name} ${
+    variables.lastname
+  }</h1>
+          <h2>${variables.role == null ? "Sin rol" : variables.role}</h2>
           <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+          <ul class="${
+            variables.socialMediaPosition != "position-left"
+              ? "position-right"
+              : ""
+          } ">
+            <li><a href="https://twitter.com/${
+              variables.twitter == null ? "4geeksacademy" : variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
@@ -56,7 +68,7 @@ window.onload = function() {
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: null,
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
